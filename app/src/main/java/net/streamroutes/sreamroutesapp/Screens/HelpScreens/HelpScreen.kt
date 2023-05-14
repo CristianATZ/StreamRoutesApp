e804a84bb36e095f9930b9716737dc3543cc9a34
CristianATZ <s20120154@alumnos.itsur.edu.mx>
2023-05-14 10:39:59 -0600

    CristianATZ - 14/05/203 - 10.30am

--- app/src/main/java/net/streamroutes/sreamroutesapp/Screens/HelpScreens/HelpScreen.kt
+++ app/src/main/java/net/streamroutes/sreamroutesapp/Screens/HelpScreens/HelpScreen.kt
@@ -1,6 +1,6 @@
 package net.streamroutes.sreamroutesapp.Screens.HelpScreens
 
-import androidx.compose.foundation.ExperimentalFoundationApi
+import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,14 +8,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -31,14 +34,22 @@ import androidx.navigation.NavController
 import net.streamroutes.sreamroutesapp.Navigation.AppScreens
 import net.streamroutes.sreamroutesapp.R
 
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
-@ExperimentalFoundationApi
 @Composable
 fun HelpScreen(navController: NavController) {
-    Column(
-        modifier = Modifier
-            .padding(top = 50.dp, start = 10.dp, end = 10.dp, bottom = 10.dp)
-    ) {
+    Column() {
+        TopAppBar(
+            title = { Text(stringResource(id = R.string.tittle_help_screen)) },
+            navigationIcon = {
+                IconButton(onClick = { navController.popBackStack()  }) {
+                    Icon(
+                        Icons.Filled.ArrowBack,
+                        contentDescription = "Regresara al menu principal"
+                    )
+                }
+            })
         // comments button
         Row (
             modifier = Modifier
