d58e4a6ad84df9d6e9995e3cf1e26ec9ea41a94b
CristianATZ <s20120154@alumnos.itsur.edu.mx>
2023-05-16 18:19:20 -0600

    CristianToZa - 16/05/2023 - 6:20pm

--- app/src/main/java/net/streamroutes/sreamroutesapp/Screens/MainScreen.kt
+++ app/src/main/java/net/streamroutes/sreamroutesapp/Screens/MainScreen.kt
@@ -3,6 +3,7 @@
 package net.streamroutes.sreamroutesapp.Screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -11,7 +12,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.rememberMarkerState
 import net.streamroutes.sreamroutesapp.Navigation.AppScreens
 
 @Composable
@@ -21,6 +30,26 @@ fun MainScreen(navController: NavController) {
 
 @Composable
 fun Main(navController: NavController){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        val singapore = LatLng(1.35,103.87)
+        val singaporeState = MarkerState(position = singapore)
+        val cameraPositionState = rememberCameraPositionState(){
+            position = CameraPosition.fromLatLngZoom(singapore,10f)
+        }
+        GoogleMap(
+            modifier = Modifier
+                .fillMaxSize(),
+            cameraPositionState = cameraPositionState
+        ){
+            Marker(
+                state = singaporeState,
+                title = "Marker in singapore"
+            )
+        }
+    }
     Column(
         modifier = Modifier
             .fillMaxSize(),
@@ -43,6 +72,31 @@ fun Main(navController: NavController){
             navController.navigate(route = AppScreens.ProfileScreen.route)
         }) {
             Text(text = "Perfil")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainView(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        val singapore = LatLng(1.35,103.87)
+        val singaporeState = MarkerState(position = singapore)
+        val cameraPositionState = rememberCameraPositionState(){
+            position = CameraPosition.fromLatLngZoom(singapore,10f)
+        }
+        GoogleMap(
+            modifier = Modifier
+                .fillMaxSize(),
+            cameraPositionState = cameraPositionState
+        ){
+            Marker(
+                state = singaporeState,
+                title = "Marker in singapore"
+            )
         }
     }
 }
