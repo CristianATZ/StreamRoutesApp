061b542e440c0112eb8fbd0f06792c5a363ac9a4
CristianATZ <98532383+CristianATZ@users.noreply.github.com>
2023-07-14 01:01:26 -0600

    CristianATZ - 14/07/2023 - 01:01am

--- app/src/main/java/net/streamroutes/sreamroutesapp/Colores/coloresScreen.kt
+++ app/src/main/java/net/streamroutes/sreamroutesapp/Colores/coloresScreen.kt
@@ -1,2 +1,21 @@
 package net.streamroutes.sreamroutesapp.Colores
 
+import androidx.compose.ui.graphics.Color
+
+val color_fondo_claro = Color(0xFFFFF7E7)
+val color_fondo_oscuro = Color(0xFF153040)
+
+val color_fondo_botones = color_fondo_oscuro
+val color_letra_botones = Color(0xFFFFFFFF)
+
+val color_fondo_textfield = Color(0xFFFFE5B4)
+val color_letra_textfield = Color(0xFFE8AA42)
+
+val color_letra = color_fondo_oscuro
+val color_letra_alterno = color_letra_textfield
+
+val color_fondo_topappbar = color_fondo_oscuro
+val color_letra_topappbar = color_letra_botones
+
+val color_fondo_radiobuttons = color_letra_textfield
+val color_fondo_checkbox = color_letra_textfield
