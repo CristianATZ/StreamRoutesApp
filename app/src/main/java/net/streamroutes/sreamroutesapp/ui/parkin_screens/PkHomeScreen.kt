9cc32f07a23f910ef76796cff5b8baf01f4ee383
CristianATZ <s20120154@alumnos.itsur.edu.mx>
2024-04-22 17:29:32 -0600

    Inicio estacionamiento casi terminado

--- app/src/main/java/net/streamroutes/sreamroutesapp/ui/parkin_screens/PkHomeScreen.kt
+++ app/src/main/java/net/streamroutes/sreamroutesapp/ui/parkin_screens/PkHomeScreen.kt
@@ -1,23 +1,32 @@
 package net.streamroutes.sreamroutesapp.ui.parkin_screens
 
-import androidx.annotation.StringRes
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Clear
+import androidx.compose.material.icons.filled.MonetizationOn
+import androidx.compose.material.icons.filled.Motorcycle
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.OutlinedTextField
@@ -26,38 +35,199 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import io.opencensus.trace.Span
 import net.streamroutes.sreamroutesapp.R
 
 @Composable
 fun ParkingHomeScreen() {
     Column {
         Header()
+        Vehicle()
+        Spots()
     }
 }
 
 @Composable
 private fun Spots() {
-    TODO("Not yet implemented")
+    Column {
+        // encabezado
+        Row {
+            Text(
+                text = stringResource(id = R.string.lblEspaciosCercanos),
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(16.dp)
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+            
+            Text(
+                text = stringResource(id = R.string.lblVerTodo),
+                style = MaterialTheme.typography.titleMedium,
+                color = colorScheme.tertiary,
+                fontWeight = FontWeight.ExtraBold,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+
+        // espacios cercanos
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(5){
+                SpotItem("Nombre del lugar", "Calle #N, Colonia, Codigo Postal", "5.0", "30", Icons.Filled.MonetizationOn)
+            }
+        }
+    }
+}
+
+@Composable
+private fun SpotItem(
+    name: String,
+    informacion: String,
+    calificacion: String,
+    precio: String,
+    foto: ImageVector
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 8.dp
+        ),
+        modifier = Modifier
+            .padding(vertical = 8.dp)
+            .fillMaxWidth(0.9f)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+
+            Icon(imageVector = foto, contentDescription = null, modifier = Modifier.size(100.dp))
+
+            Column(
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Spacer(modifier = Modifier.size(8.dp))
+
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(vertical = 4.dp)
+                )
+                Text(
+                    text = informacion,
+                    style = MaterialTheme.typography.titleSmall,
+                    fontWeight = FontWeight.Bold,
+                    letterSpacing = 2.sp,
+                    modifier = Modifier
+                        .padding(vertical = 4.dp)
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Star,
+                        contentDescription = null,
+                        tint = colorScheme.tertiary,
+                        modifier = Modifier
+                            .size(25.dp)
+                    )
+                    Text(
+                        text = calificacion,
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.Bold
+                    )
+                    
+                    Spacer(modifier = Modifier.weight(1f))
+                    
+                    Text(
+                        text = "$$precio",
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Bold
+                    )
+                    
+                    Spacer(modifier = Modifier.size(16.dp))
+                }
+            }
+        }
+    }
 }
 
 @Composable
 private fun Vehicle() {
-    TODO("Not yet implemented")
+    Column {
+        // encabezado
+        Row {
+            Text(
+                text = stringResource(id = R.string.lblMisVehiculos),
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+
+        // vehiculos del usuario
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth(0.95f)
+        ) {
+            if( true ){
+                items( 5/*Aqui agregar el viewmodel*/){
+                    VehicleItem("58ECP3", Icons.Filled.Motorcycle)
+                }
+            } else {
+
+            }
+        }
+    }
+}
+
+@Composable
+private fun VehicleItem(
+    name: String,
+    icono: ImageVector
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 8.dp
+        ),
+        modifier = Modifier
+            .padding(horizontal = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .size(100.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = icono,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(50.dp)
+            )
+            Text(
+                text = name,
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.SemiBold
+            )
+        }
+    }
 }
 
 @Composable
@@ -87,7 +257,7 @@ private fun Header() {
             .fillMaxWidth()
             .height(250.dp)
             .background(
-                color = colorScheme.onBackground,
+                color = colorScheme.primary,
                 RoundedCornerShape(bottomEnd = 16.dp, bottomStart = 16.dp)
             ),
         verticalArrangement = Arrangement.Center,
