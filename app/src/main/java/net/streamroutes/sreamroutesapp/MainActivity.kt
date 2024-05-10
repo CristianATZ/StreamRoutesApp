cbd59cc4b7fbab2846266798f1fa07f25678acae
CristianATZ <s20120154@alumnos.itsur.edu.mx>
2024-05-10 00:24:19 -0600

    StatusBar y NavigationBar Color cambiados

--- app/src/main/java/net/streamroutes/sreamroutesapp/MainActivity.kt
+++ app/src/main/java/net/streamroutes/sreamroutesapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package net.streamroutes.sreamroutesapp
 
+import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -14,12 +15,13 @@ import net.streamroutes.sreamroutesapp.ui.Theme.RumaAppTheme
 import net.streamroutes.sreamroutesapp.utils.MyViewModel
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("NewApi")
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val myViewModel : MyViewModel = viewModel()
-            RumaAppTheme (myViewModel.tema){
+            RumaAppTheme (false){
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize()
--- app/src/main/java/net/streamroutes/sreamroutesapp/ui/Theme/Theme.kt
+++ app/src/main/java/net/streamroutes/sreamroutesapp/ui/Theme/Theme.kt
@@ -10,10 +10,13 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import net.streamroutes.sreamroutesapp.viewmodel.routes.ConfigurationViewModel
 
 private val LightColorScheme = lightColorScheme(
     primary = md_theme_light_primary,
@@ -84,31 +87,27 @@ private val DarkColorScheme = darkColorScheme(
 @Composable
 fun RumaAppTheme(
         darkTheme: Boolean = isSystemInDarkTheme(),
-        // Dynamic color is available on Android 12+
-        dynamicColor: Boolean = true,
         content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        /*dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }*/
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = colorScheme.tertiary.toArgb()
+            window.navigationBarColor = Color(0xFFEACE43).toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
 
     MaterialTheme(
-            colorScheme = colorScheme,
-            typography = Typography,
-            content = content
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
     )
 }
